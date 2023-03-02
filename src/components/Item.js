dc501a3175a7e36ae00ed0fc3db831fa5a2ac822
Aurore Blaison <aurore_blaison@hotmail.fr>
2023-03-02 13:04:40 +0100

    :lipstick: navbar design / pas de responsive / manque les ancres vers les sections / redirection vers page profil ok

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../assets/sass/item.scss';
 import Footer from './Footer';
+import Navbar from './Navbar';
 
 const Item = () => {
   return (
     <div>
+      <Navbar/>
       test Item
       <Footer />
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import '../assets/sass/navbar.scss';
+import logo from '../assets/img/Logo.png'
+import { BsPersonCircle } from "react-icons/bs"
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const goToProfil = ()=>{
+    navigate("/profil");
+  }
+
   return (
-    <div>Navbar</div>
+    <div className="navbar-container">
+
+      <div className="logo-wrapper">
+        <img src={logo} alt="logo Five star's Booking" />
+      </div>
+
+      <ul className="navbar">
+        <li> <a href="">à propos de nous</a> </li>
+        <li> <a href="">réservation</a> </li>
+        <li> <a href="">contact</a> </li>
+      </ul>
+
+      <div className="button" onClick={() => goToProfil()}>
+        <BsPersonCircle />
+      </div>
+
+    </div>
   )
 }
 
