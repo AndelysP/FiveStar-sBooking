6f22e86e2418a102aefd860644f2f2a43a037f52
Aurore Blaison <aurore_blaison@hotmail.fr>
2023-03-02 09:55:11 +0100

    :lipstick: finalisation style Footer + responsive ok COMPOSANT DISPO

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,9 @@ import "../assets/sass/footer.scss";
 const Footer = () => {
   return (
     <div className='footer'>
-      &#x24D2; 2023 Five Star's Booking All rights reserved
+      <p>
+        &#x24D2; 2023 Five Star's Booking All rights reserved
+      </p>
     </div>
   )
 }
