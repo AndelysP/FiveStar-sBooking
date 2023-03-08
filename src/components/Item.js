9449d3d301fcdce7a815823ad45c8455d5a33857
Aurore Blaison <aurore_blaison@hotmail.fr>
2023-03-08 16:58:42 +0100

    :lipstick: page item avancée (plus de bug d'affichage) + une partie du style

--- src/components/Item.js
+++ src/components/Item.js
@@ -12,35 +12,134 @@ const Item = () => {
   // On récupère l'id de l'item cliqué 
   const getItem = async () => {
     await fetch(`http://localhost:5500/ships/${id}`)
-    .then(response => response.json())
-    .then(data => setItem(data));
+      .then(response => response.json())
+      .then(data => setItem(data));
   }
 
   useEffect(() => {
     getItem()
   }, []);
-  
+
   // S'il n'y a rien dans le state item
   if (!item) {
-    return <div>Page en cours de chargement...</div>;
+    return (
+      <div>
+        <div className="navbar-wrapper">
+          <Navbar />
+        </div>
+
+        <h1>Page en cours de chargement</h1>
+
+        <Footer />
+      </div>
+    )
   }
 
   return (
     <>
+    <div className="navbar-wrapper">
       <Navbar />
-      <div>
-        <h1>{item.name}</h1>
-        <p>{item.description}</p>
+    </div>
 
-        <img src={require("../assets/img/ships/Ship/" + item.name + "_Ship.png")} alt={item.name} />
+    <div className="cover">
+    <img src={require("../assets/img/ships/Ship/" + item.name + "_Ship.png")} alt={item.name} />
+      <h1>{item.name}</h1>
+    </div>
 
-        <img src={require("../assets/img/ships/Bathroom/" + item.name + "_Bathroom.png")} alt={item.name} />
+    {/* formulaire filtre a ajouter ici */}
 
-        <img src={require("../assets/img/ships/Bedroom/" + item.name + "_Bedroom.png")} alt={item.name} />
+    <div className="ship-details">
 
-        <img src={require("../assets/img/ships/Inside/" + item.name + "_Inside.png")} alt={item.name} />
+      <div className="wrapper1">
+        <div className="ship-pictures">
+
+          <div className="main-pic">
+          <img src={require("../assets/img/ships/Ship/" + item.name + "_Ship.png")} alt={item.name} />
+          </div>
+
+          {/* utilisation du composant carroussel antD */}
+
+          <div className="rooms-pic">
+          <img src={require("../assets/img/ships/Bedroom/" + item.name + "_Bedroom.png")} alt={item.name} />
+            <img src={require("../assets/img/ships/Bathroom/" + item.name + "_Bathroom.png")} alt={item.name} />
+            <img src={require("../assets/img/ships/Inside/" + item.name + "_Inside.png")} alt={item.name} />
+            <img src={require("../assets/img/ships/Kitchen/" + item.name + "_Kitchen.png")} alt={item.name} />
+            <img src={require("../assets/img/ships/Livingroom/" + item.name + "_Livingroom.png")} alt={item.name} />
+          </div>
+        </div>
+
+        <div className="ship-price">
+          <h1> prix</h1>
+          <p>{item.price}€</p>
+        </div>
+
+        <div className="ship-foodOptions">
+          <h1> Option repas premium</h1>
+          <p>300€</p>
+        </div>
+
+        <div className="ship-EntertainmentOption">
+          <h1> option divertissement</h1>
+          <p>150€ (comprends l'acces a des films, jeux vidéos, espace d'entrainement...)</p>
+        </div>
       </div>
-      <Footer />
+
+      <div className="wrapper2">
+
+        <div className="ship-description">
+          <p>{item.description}</p>
+        </div>
+
+        <div className="ship-equipments">
+          <h1> équipements</h1>
+          <ul>
+            <li> {item.equipment.bedroom} chambre(s) </li>
+            <li> {item.equipment.bathroom} salle(s) de bain</li>
+            <li>{item.equipment.livingroom} salon(s)</li>
+          </ul>
+        </div>
+
+      </div>
+
+    </div>
+
+    <button >Réserver !</button>
+
+    <div className="schedule">
+      <div className="step">
+        <img src={require("../assets/img/icons/earth.png")} alt="" />
+        <p>
+          Terre <br /> Notre point de départ.
+        </p>
+      </div>
+      <div className="step">
+        <img src={require("../assets/img/icons/constellation.png")} alt="" />
+        <p>
+          Un passage par les plus belles constellations de l'espace.
+        </p>
+      </div>
+      <div className="step">
+        <img src={require("../assets/img/icons/saturn.png")} alt="" />
+        <p>
+          Vous aurez l'occasion de voir de plus pres les anneaux de Saturne
+        </p>
+      </div>
+      <div className="step">
+        <img src={require("../assets/img/icons/base.png")} alt="" />
+        <p>
+          Le chemin se poursuit par la visite d'une station spatiale pour la collecte des données.
+        </p>
+      </div>
+      <div className="step">
+        <img src={require("../assets/img/icons/exoplanet.png")} alt="" />
+        <p>
+          Votre voyage s'achèvera en passant par une toute nouvelle exoplanète découverte : Kepler 22b.
+        </p>
+      </div>
+
+    </div>
+
+    <Footer />
     </>
   )
 }
