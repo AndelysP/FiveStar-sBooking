5ebeeef6fabe2f4fede6537d8116506f7b627ca7
Cassandre Lelong <cassandre.lelong@gmail.com>
2023-03-22 13:59:56 +0100

    Création de la page profil une fois l'utilisateur connecté

--- src/components/ProfilConnect.js
+++ src/components/ProfilConnect.js
@@ -2,18 +2,27 @@ import Footer from './Footer';
 import Navbar from './Navbar';
 import React, { useState, useEffect } from 'react';
 import profilLogin from '../assets/img/profil_login.jpg';
+import { useNavigate } from 'react-router-dom';
 import '../assets/sass/profilConnect.scss';
 
 
 const ProfilConnect = () => {
 
-    const API = "http://localhost:5500/users";
-    const [data, setData] = useState([]); // données du JSON
+    const API = "http://localhost:5500/users/6414190d05aa9f3fba3d4b14";
+    const [user, setUser] = useState({}); // données du JSON
+
+    const [avatar, setAvatar] = useState("");
+
+    const navigate = useNavigate();
+
+    const goToHome = () => {
+        navigate("/home");
+    }
 
     const getUser = async () => {
         await fetch(`${API}`)
             .then(res => res.json())
-            .then(data => setData(data))
+            .then(data => setUser(data))
             .catch(error => {
                 console.log(error);
             });
@@ -23,6 +32,22 @@ const ProfilConnect = () => {
         getUser()
     }, []);
 
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append('avatar', avatar.file)
+
+
+
+        await fetch("http://localhost:5500/users/6414190d05aa9f3fba3d4b14", {
+            method: "POST",
+            body: formData
+        }).then(res => res.json())
+            .then(json => setUser({ ...user, avatar: json.avatar }));
+    }
+
+    const uploadsUrl = process.env.REACT_APP_UPLOADS_URL;
     return (
         <div>
             <Navbar />
@@ -32,16 +57,31 @@ const ProfilConnect = () => {
 
             <div className="helloProfil">
 
-                {data.map((user) => (
-                    <>
-                        <h1>Bonjour {user.userfirstname}</h1>
 
-                        <p>Prêt.e à embarquer avec nous ?</p>
+                {/* Affichage des données de l'utilisateur */}
+                <div key={user._id} className="partieGauche">
+                    <div className="avatar">
+                        <img src={`${uploadsUrl}${user.avatar}`} alt="avatar utilisateur" />
+                    </div>
 
-                        <button type='button' className="password-btn">Réservez un voyage</button>
-                    </>
-                ))}
+                    <div className="uploadAvatar">
+                        {/* Formulaire d'ajout d'une image de profil */}
+                        <form onSubmit={handleSubmit} encType="multipart/form-data">
+                            <p>Ajouter ou modifier mon avatar :</p>
+                            <input type="file" name="avatar" onChange={(e) => setAvatar({ file: e.target.files[0] })} />
+                            <button type='submit' className="uploadAvatar password-btn" >Ajouter</button>
+                        </form>
+                    </div>
+                </div>
 
+
+                <div className="partieDroite">
+                    <h1>Bonjour {user.userfirstname}</h1>
+
+                    <p>Prêt.e à embarquer avec nous ?</p>
+
+                    <button type='button' className="password-btn" onClick={() => goToHome()}>Réservez un voyage</button>
+                </div>
             </div>
 
             <Footer />
